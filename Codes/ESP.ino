5f6267a69106a68ba352bbf2695077756329460a
Ata Dibazar <118345682+Atadbz@users.noreply.github.com>
2023-04-04 15:26:38 +0330

    Update ESP.ino

--- Codes/ESP.ino
+++ Codes/ESP.ino
@@ -1,29 +1,44 @@
 
+#include<ESP32Servo.h>
+Servo s1;
 
 int soilmoisture1 = 0;
-int soilmoisture2 = 0;
-int soilmoisture3 = 0;
 
+int count=0;
 
 
 void setup()
 {
-  //pinMode(A0, INPUT);
-  //pinMode(A1, INPUT);
-  //pinMode(A2, INPUT);
-  //s1.attach(9);
-  pinMode(34,INPUT);
+  pinMode(13, INPUT);
+ pinMode(12, OUTPUT);
+  s1.attach(12);
   Serial.begin(9600);
-  pinMode(9, OUTPUT);
+  
 }
 
 
 void loop()
 {
-  soilmoisture1 = analogRead(34);
-
+  soilmoisture1 = analogRead(13);
+ // soilmoisture2 = analogRead(A1);
+ // soilmoisture3 = analogRead(A2);
   Serial.println(soilmoisture1);
-  delay(1000);
+ // Serial.println(soilmoisture2);
+ // Serial.println(soilmoisture3);
   
-}
+  if (soilmoisture1 >200) {
+    
+    
+    s1.write(0);
+    delay(1000);
+    s1.write(180);
+    delay(1000);
+    s1.write(0);
 
+  } else {
+    digitalWrite(9, LOW);
+  }
+  
+  
+  delay(10); // Delay a little bit to improve simulation performance
+}
