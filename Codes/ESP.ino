3220965ba48f3281716e300c41e7645b7b15734d
Taha Farshbaf <t_farshbaf@outlook.com>
2023-04-04 11:05:02 +0330

    Update ESP.ino

--- Codes/ESP.ino
+++ Codes/ESP.ino
@@ -1,29 +1,33 @@
-
-
-int soilmoisture1 = 0;
-int soilmoisture2 = 0;
-int soilmoisture3 = 0;
-
-
-
-void setup()
-{
-  //pinMode(A0, INPUT);
-  //pinMode(A1, INPUT);
-  //pinMode(A2, INPUT);
-  //s1.attach(9);
-  pinMode(34,INPUT);
-  Serial.begin(9600);
-  pinMode(9, OUTPUT);
+#include <Servo.h>
+Servo s1;
+unsigned int plots[] = { 12, 13, 14 };
+int i;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(12, INPUT);
+  pinMode(13, INPUT);
+  pinMode(14, INPUT);
+  s1.attach(2);
 }
 
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i = 0; i <= 2; i++) {
+    Serial.print("Plot: ");
+    Serial.print(i + 1);
+    Serial.print(" = ");
+    Serial.print(analogRead(plots[i]));
+  }
 
-void loop()
-{
-  soilmoisture1 = analogRead(34);
-
-  Serial.println(soilmoisture1);
-  delay(1000);
-  
+  for (int i = 0; i <= 2; i++) {
+    if (analogRead(plots[i]))
+      popm_active(i);
+  }
 }
 
+void popm_active(int i) {
+  Serial.print("Turning to ");
+  Serial.print(i + 1);
+  s1.write(i * 90);
+}
